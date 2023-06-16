039b831f1913335f11c691366aa0c828c09338d1
Wojtek Witkowski <hi@wojtek.im>
2023-06-16 12:40:15 +0200

    include contentHash in the response

--- express/src/types.ts
+++ express/src/types.ts
@@ -1,44 +1,45 @@
-import { DecodedContentHash } from '@ensdomains/ensjs/utils/contentHash'
+import { DecodedContentHash } from "@ensdomains/ensjs/utils/contentHash";
 
 export type EnsjsProfile = {
-  isMigrated: boolean | null
-  createdAt: string | null
-  address?: string | undefined
-  name?: string | null | undefined
-  decryptedName?: string | null | undefined
-  match?: boolean | undefined
-  message?: string | undefined
+  isMigrated: boolean | null;
+  createdAt: string | null;
+  address?: string | undefined;
+  name?: string | null | undefined;
+  decryptedName?: string | null | undefined;
+  match?: boolean | undefined;
+  message?: string | undefined;
   records?:
     | {
-        contentHash?: string | DecodedContentHash | null | undefined
+        contentHash?: string | DecodedContentHash | null | undefined;
         texts?:
           | {
-              key: string | number
-              type: 'text' | 'addr' | 'contentHash'
-              coin?: string | undefined
-              value: string
+              key: string | number;
+              type: "text" | "addr" | "contentHash";
+              coin?: string | undefined;
+              value: string;
             }[]
-          | undefined
+          | undefined;
         coinTypes?:
           | {
-              key: string | number
-              type: 'text' | 'addr' | 'contentHash'
-              coin?: string | undefined
-              value: string
+              key: string | number;
+              type: "text" | "addr" | "contentHash";
+              coin?: string | undefined;
+              value: string;
             }[]
-          | undefined
+          | undefined;
       }
-    | undefined
-  resolverAddress?: string | undefined
-  isInvalidResolverAddress?: boolean | undefined
-  reverseResolverAddress?: string | undefined
-}
+    | undefined;
+  resolverAddress?: string | undefined;
+  isInvalidResolverAddress?: boolean | undefined;
+  reverseResolverAddress?: string | undefined;
+};
 
 export type FormattedProfile = {
-  name?: string
-  address?: string
-  avatar?: string | null
+  name?: string;
+  address?: string;
+  avatar?: string | null;
+  contentHash?: string | DecodedContentHash | null;
   records?: {
-    [key: string | number]: string
-  }
-}
+    [key: string | number]: string;
+  };
+};
--- express/src/utils.ts
+++ express/src/utils.ts
@@ -1,84 +1,76 @@
-import { EnsjsProfile, FormattedProfile } from './types.js'
-import { AvatarResolver } from '@ensdomains/ens-avatar'
-import { JSDOM } from 'jsdom'
-import { provider } from './provider.js'
+import { EnsjsProfile, FormattedProfile } from "./types.js";
+import { AvatarResolver } from "@ensdomains/ens-avatar";
+import { JSDOM } from "jsdom";
+import { provider } from "./provider.js";
 
-const window = new JSDOM().window
-const avt = new AvatarResolver(provider)
-const matcherIpfs = new RegExp('^(ipfs)://(.*)$', 'i')
-const zeroAddress = '0x0000000000000000000000000000000000000000'
+const window = new JSDOM().window;
+const avt = new AvatarResolver(provider);
+const matcherIpfs = new RegExp("^(ipfs)://(.*)$", "i");
+const zeroAddress = "0x0000000000000000000000000000000000000000";
 
 export async function formatProfile(
   profile: EnsjsProfile | undefined,
   addressOrName: string
 ): Promise<FormattedProfile> {
   // Return empty object if no profile
-  if (
-    !profile ||
-    profile.resolverAddress === zeroAddress ||
-    profile.message === "Name doesn't have a resolver"
-  ) {
-    return {}
+  if (!profile || profile.resolverAddress === zeroAddress || profile.message === "Name doesn't have a resolver") {
+    return {};
   }
 
   // If both address and name are null, it can't an ENS name
   if (!profile.name && !profile.address) {
-    return {}
+    return {};
   }
 
   // Fill in the name or address from the request (ensjs doesn't return it)
-  if (!profile.name) profile.name = addressOrName
-  if (!profile.address) profile.address = addressOrName
+  if (!profile.name) profile.name = addressOrName;
+  if (!profile.address) profile.address = addressOrName;
 
-  const avatarRecord = profile.records?.texts?.find(
-    (record) => record.key === 'avatar'
-  )?.value
+  const avatarRecord = profile.records?.texts?.find((record) => record.key === "avatar")?.value;
 
   return {
     name: profile.name!,
     address: profile.address!,
     avatar: await encodeAvatar(profile.name!, avatarRecord),
+    contentHash: profile.records?.contentHash!,
     records:
       profile.records?.texts?.reduce((acc, record) => {
-        acc![record.key] = record.value
-        return acc
-      }, {} as FormattedProfile['records']) || {},
-  }
+        acc![record.key] = record.value;
+        return acc;
+      }, {} as FormattedProfile["records"]) || {},
+  };
 }
 
-async function encodeAvatar(
-  name: string,
-  avatarRecord: string | undefined
-): Promise<string | null> {
-  if (!avatarRecord) return null
+async function encodeAvatar(name: string, avatarRecord: string | undefined): Promise<string | null> {
+  if (!avatarRecord) return null;
 
-  if (avatarRecord.startsWith('http') || avatarRecord.startsWith('data')) {
+  if (avatarRecord.startsWith("http") || avatarRecord.startsWith("data")) {
     // if the avatar record is a URL or raw data, return it as-is to avoid an async call
-    return avatarRecord
+    return avatarRecord;
   } else if (avatarRecord.match(matcherIpfs)) {
     // if the avatar record is an IPFS hash, turn it into a URL
     try {
-      return getIpfsLink(avatarRecord)
+      return getIpfsLink(avatarRecord);
     } catch (err) {
-      console.error(err)
-      return null
+      console.error(err);
+      return null;
     }
   } else {
     // sometimes we need an async call to get the avatar (e.g. if it's an NFT)
     return await avt.getAvatar(name, {
       jsdomWindow: window,
-    })
+    });
   }
 }
 
 export function getIpfsLink(link: string): string {
   if (link.match(/^ipfs:\/\/ipfs\//i)) {
-    link = link.substring(12)
+    link = link.substring(12);
   } else if (link.match(/^ipfs:\/\//i)) {
-    link = link.substring(7)
+    link = link.substring(7);
   } else {
-    throw new Error('Invalid IPFS link')
+    throw new Error("Invalid IPFS link");
   }
 
-  return `https:/\/ipfs.io/ipfs/${link}`
+  return `https:/\/ipfs.io/ipfs/${link}`;
 }
